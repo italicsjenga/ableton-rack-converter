fd344e25478797b3515446f02617b6fa37f6153b
Alex Janka <alex@alexjanka.com>
2021-07-10 14:00:37 +1000

    use relative paths

--- src/bin/converter.rs
+++ src/bin/converter.rs
@@ -1,8 +1,8 @@
 use ableton_rack_converter::{self, fixers};
 
 fn main() {
-    let file_load = String::from("C:\\Users\\janka\\Documents\\Projects\\Programming\\ableton-rack-converter\\validation\\Utility10.adg");
-    let file_save = String::from("C:\\Users\\janka\\Documents\\Projects\\Programming\\ableton-rack-converter\\validation\\Utility10_save.xml");
+    let file_load = String::from(".\\validation\\Utility10.adg");
+    let file_save = String::from(".\\exported_files\\Utility10_save.xml");
     let mut device = ableton_rack_converter::load_adg(&file_load);
     fixers::traverse_children(&mut device);
     ableton_rack_converter::save_uncompressed(&device, &file_save);
