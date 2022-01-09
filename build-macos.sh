38b04ced376515c38f34b9df45a2a78a5ad36e4e
Alex Janka <alex@alexjanka.com>
2022-01-09 18:29:30 +1100

    fixed to cross-compile from linux

--- build-macos.sh
+++ build-macos.sh
@@ -1,5 +1,7 @@
-cargo build --target-dir=./build/macos --release
+#!/bin/bash
+# set -e 
+cargo build --target=x86_64-apple-darwin --target-dir=./build/macos --release
 mkdir -p build/release/macos
-cp build/macos/release/converter build/release/macos/ableton-rack-converter
-cp build/macos/release/compressor build/release/macos/xml-to-rack
-cp build/macos/release/decompressor build/release/macos/rack-to-xml
+cp build/macos/x86_64-apple-darwin/release/converter build/release/macos/ableton-rack-converter
+cp build/macos/x86_64-apple-darwin/release/compressor build/release/macos/xml-to-rack
+cp build/macos/x86_64-apple-darwin/release/decompressor build/release/macos/rack-to-xml
--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+# set -e 
 rm -rf build/release
 mkdir -p build/release
 ./build-macos.sh
 ./build-windows.sh
 cd build/release
 zip -j macos-ableton-rack-converter.zip macos/*
-zip -j windows-ableton-rack-converter.zip windows/*
+zip -j windows-ableton-rack-converter.zip windows/*.exe
