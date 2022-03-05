68f895adf4b0fa164a2884cda67f3b85a97bb555
Alex Janka <alex@alexjanka.com>
2022-03-05 15:18:10 +1100

    fix build script

--- build-windows.sh
+++ build-windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set -e 
-cargo build --target=x86_64-pc-windows-gnu --target-dir=./build/release/windows --release
+cargo build --target=x86_64-pc-windows-gnu --target-dir=./build/windows --release
 mkdir -p build/release/windows
 cp build/windows/x86_64-pc-windows-gnu/release/converter.exe build/release/windows/ableton-rack-converter.exe
 cp build/windows/x86_64-pc-windows-gnu/release/compressor.exe build/release/windows/xml-to-rack.exe
